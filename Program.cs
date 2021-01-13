87e06e08035a7fec389f54feaa8bb42235acf697
Kingsley <kins4swagg@gmail.com>
2021-01-13 21:35:40 +0100

    Basic Syntax CSharp

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+
+
+
+    class Rectangle
+    {
+        //class members
+        double length;
+        double width;
+
+        //This method accepts rectangle details
+        public void AcceptDetails()
+        {
+            length = 10.6;
+            width = 17.5;
+        }
+
+        //This gets rectangle area
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        //This displays the details of a rectangle
+        public void Display()
+        {
+            Console.WriteLine($"This is length:{length}");
+            Console.WriteLine($"This is width:{width}");
+            Console.WriteLine($"This is the area of the rectangle:{GetArea()}");
+        }
+
+
+
+    }
+
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.AcceptDetails();
+            rectangle.Display();
+
+        }
+    }
+}
